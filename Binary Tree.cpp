676bad64ea10db70cd3f9d66271ba2e88535c88e
derik0314 <1762424183@qq.com>
2024-03-27 13:43:01 +0800

    二叉树更新
    加入了搜索二叉树、堆

--- Binary Tree.cpp
+++ Binary Tree.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-//二叉树节点
+//二叉树节点--类定义
 template <typename T>
 class BinaryTreeNode{
     friend class BinaryTree <T>;
@@ -21,11 +21,11 @@ class BinaryTreeNode{
         bool isLeaf() const;
 };
 
-//二叉树
+//二叉树--类定义
 template <typename T>
 class BinaryTree{
     private:
-        BinaryTreeNode<T>* m_root;
+        BinaryTreeNode<T>* root;
     public:
         BinaryTree();
         ~BinaryTree();
@@ -123,3 +123,183 @@ void BinaryTree<T>::DeleteBinaryTree(BinaryTreeNode<T>* root){
         delete root;
     }
 }
+
+//二叉搜索树--类定义
+template<typename T>
+class BinarySearchTree{
+    private:
+        BinaryTreeNode<T>* root;
+    public:
+        BinaryTreeNode<T>* Find(BinaryTreeNode<T>* cur);
+        void InsertTreeNode(BinaryTreeNode<T>* root, 
+                            BinaryTreeNode<T>* newPointer);
+};
+
+//二叉搜索树的插入
+template<typename T>
+void BinarySearchTree<T>::InsertTreeNode(BinaryTreeNode<T>* root, BinaryTreeNode<T>* newPointer){
+    BinaryTreeNode<T>* pointer = 0;
+    if(root == 0){
+        root = newPointer;
+        return;
+    }else{
+        pointer = root;
+        while(pointer != 0){
+            if(newPointer->value() == pointer->value())
+                return;
+            else if(newPointer->value() < pointer->value()){
+                if(pointer->leftChild == 0){
+                    pointer->leftChild = newPointer;
+                    return;
+                }else   pointer = pointer->leftChild; 
+            }else{
+                if(pointer->rightChild == 0){
+                    pointer->rightChild = newPointer;
+                    return;
+                }else   pointer = pointer->rightChild;
+            }
+        }
+    }
+}
+
+//堆--类定义
+template < typename T >
+class MinHeap{
+    private:
+        T* heapArray;		//存放堆数据的数组
+        int curSize;		//当前堆中元素数目
+        int maxSize;		//堆所能容纳的最大元素数目
+        void BuildHeap();	//建堆操作
+    public:
+        MinHeap(const int n);
+        virtual ~MinHeap();
+        bool IsLeaf(int pos)const;
+        int	LeftChild(int pos)const;
+        int	RightChild(int pos)const;
+        int	Parent(int pos)const;
+        bool Remove(int pos, T& node);
+        bool Insert(const T& newnode);
+        T& RemoveMin();
+        void SiftUp(int pos); 	//向上筛
+        void SiftDown(int pos);//向下筛
+};
+
+//堆的构造函数
+template < typename T >
+MinHeap< T >::MinHeap(const int n){
+	if( n <= 0) return;
+	this->curSize = 0;
+	this->maxSize = n;//初始化容量为n
+	this->heapArray = new T[this->maxSize];
+	this->buildHeap();
+}
+
+//判断是否为叶节点
+template < typename T >
+bool MinHeap< T >::IsLeaf(int pos) const {
+	return (pos >= this->curSize/2) && //该位置处于堆的下半部分
+		 (pos <= this->curSize-1); //堆的索引从0开始
+}
+
+//返回父节点位置
+template < typename T >
+int MinHeap< T >::Parent(int pos) const {
+	return (pos-1)/2;
+}
+
+//返回左子节点位置
+template < typename T >
+int MinHeap< T >::LeftChild(int pos)const {
+	return pos*2+1;
+}
+
+//返回右子节点位置
+template < typename T >
+int MinHeap< T >::RightChild(int pos) const {
+	return pos*2+2;
+}
+
+//建初堆
+template < typename T >
+void MinHeap< T >::BuildHeap(){
+	for( int i = this->curSize/2-1; i>=0; i-- ){
+		this->siftDown(i);//反复调用进行筛选
+	}
+}
+
+//向下筛选
+template < typename T >
+void MinHeap< T >::SiftDown(int pos){
+	int i = pos;  //标识父节点位置
+	int j = this->LeftChild(i);  //获得左子节点位置
+	T temp = this->heapArray[i];  //临时存储父节点的值
+	while( j < this->curSize){	
+		if((j < this->curSize-1) && 
+		   (this->heapArray[j] > this->heapArray[j+1])){//如果右子节点更小
+			j++;  //令j为右子节点的位置
+		}
+		if(temp > this->heapArray[j]){//如果父节点大于子节点值
+			this->heapArray[i] = this->heapArray[j];  //子节点值上移
+			i = j;  //原父节点位置更新为子节点位置
+			j = this->LeftChild(j);
+		}else break;   
+	}
+	this->heapArray[i] = temp;
+}
+
+//向上筛选
+template < typename T >
+void MinHeap< T >::SiftUp(int pos){
+	int tempPos = pos;
+	T temp = this->heapArray[tempPos];
+	while((tempPos > 0) && 
+	  (this->heapArray[this->parent(tempPos)] > temp)){
+		this->heapArray[tempPos]  
+        = this->heapArray[this->parent(tempPos)];
+		tempPos = this->parent(tempPos);
+	}
+	this->heapArray[tempPos] = temp;
+}
+
+//插入新元素
+template < typename T >
+bool MinHeap< T >::Insert( const T& newnode){
+	if(this->curSize == this->maxSize)
+		return false;
+	this->heapArray[this->curSize] = newnode;  	//将新元素存入堆存储数组最后一个
+	this->siftUp(this->curSize);	//向上调整
+	this->curSize ++;
+	return true;
+}
+
+//移除最小值
+template < typename T >
+T& MinHeap< T >::RemoveMin(){
+	if(this->curSize == 0 ){
+		std::cout<<"No item to delete"<<std::endl;
+		exit(1);
+	}else{
+		this->swap(0, --this->curSize);//交换堆顶和堆尾元素
+		if(this->curSize > 1){
+			this->siftDown(0); //向下筛选，重新排序
+		}
+		return this->heapArray[this->curSize]; 
+        //返回要被删除的最小元素（堆尾元素、最初的堆首元素）
+	}
+}
+
+//删除元素
+template < typename T >
+bool MinHeap< T >::Remove(int pos, T& node){
+	if(( pos < 0) || ( pos >= this->curSize))
+		return false;
+	node = this->heapArray[pos];
+	this->heapArray[pos] = this->heapArray[--this->curSize];
+    // 更新当前堆的大小后将堆中最后一个节点的值赋给指定位置的节点
+	if(this->heapArray[this->parent(pos)] > this->heapArray[pos])
+	    this->siftUp(pos);
+	else this->siftDown(pos);
+	return true;
+}
+
+
